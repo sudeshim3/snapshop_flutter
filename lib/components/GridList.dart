7a464ee2abaeb816ed0c0d127e756c5bedd94821
sudesh-hubbler <sudesh@hubbler.mobi>
2019-05-24 14:24:19 +0530

    1. completed gridview for recent product.

--- lib/components/GridList.dart
+++ lib/components/GridList.dart
@@ -5,26 +5,18 @@ class GridListProduct extends StatefulWidget {
   _GridListProductState createState() => _GridListProductState();
 }
 
-class product {
+List products = new List();
+
+class Product {
   final String name;
   final String pictures;
   final double old_price;
   final double price;
 
-  product(this.name, this.pictures, this.old_price, this.price);
+  Product(this.name, this.pictures, this.old_price, this.price);
 }
 
-
 class _GridListProductState extends State<GridListProduct> {
-
-   List products = [
-    product("Blazers", "images/products/blazer1.jpeg", 185.0, 150.0),
-    product("Blazers Red", "images/products/blazer2.jpeg", 185.0, 150.0),
-    product("Blazers blue", "images/products/blazer1.jpeg", 185.0, 150.0),
-    product("Blazers xd", "images/products/blazer1.jpeg", 185.0, 150.0),
-    product("Blazers xl", "images/products/blazer1.jpeg", 185.0, 150.0)
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,66 +29,97 @@ class _GridListProductState extends State<GridListProduct> {
                 "Recent Product",
                 style: TextStyle(fontSize: 21.0),
               )),
-          Container(
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0,),
-                GridView.count(crossAxisCount: 2,
-                scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  childAspectRatio: 3,
-                  physics: NeverScrollableScrollPhysics(),
-                  children: <product>[
-                  product("Blazers", "images/products/blazer1.jpeg", 185.0, 150.0),
-                  product("Blazers Red", "images/products/blazer2.jpeg", 185.0, 150.0),
-                  product("Blazers blue", "images/products/blazer1.jpeg", 185.0, 150.0),
-                  product("Blazers xd", "images/products/blazer1.jpeg", 185.0, 150.0),
-                  product("Blazers xl", "images/products/blazer1.jpeg", 185.0, 150.0),
-                  product("Blazers xl", "images/products/blazer1.jpeg", 185.0, 150.0),
-                ].map((product item){
-                  return Container(
-                      color: Colors.blueAccent,
-                      width: 50.0,
-                      height: 50.0,
-                      child: Text(item.name));
-                }).toList(),)
-              ],
-            ),
-          )
+          SizedBox(
+            height: 20.0,
+          ),
+          GridRecentItems()
         ],
       ),
     );
+  }
 }
+
+class GridRecentItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    products
+        .add(Product("Blazers", "images/products/blazer1.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers Red", "images/products/blazer2.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/dress1.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers green", "images/products/dress2.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/hils1.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/hils2.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/pants1.jpg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/pants2.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/shoe1.jpg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/skt1.jpeg", 185.0, 150.0));
+    products.add(
+        Product("Blazers blue", "images/products/skt2.jpeg", 185.0, 150.0));
+    return Container(
+      child: GridView.builder(
+        shrinkWrap: true,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        scrollDirection: Axis.vertical,
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (BuildContext context, int index) {
+          Product it = products[index];
+          return SingleProduct(
+              name: it.name,
+              pictures: it.pictures,
+              old_price: it.old_price,
+              price: it.price);
+        },
+        itemCount: 4,
+      ),
+    );
+  }
 }
-
-
-/*
-* return Container(
-
-        child: Column(
-
-            children: <Widget>[
-
-
-    GridView.count(crossAxisCount: 2,
-    children: <product>[
-
-    ].map((product url) {
-    return new GridTile(
-    child: new Image.network(url.pictures, fit: BoxFit.cover);
-    }).toList())])*/
 
 class SingleProduct extends StatelessWidget {
   final String name, pictures;
   final double old_price, price;
 
-  SingleProduct({this.name, this.pictures, this.old_price, this.price });
+  SingleProduct({this.name, this.pictures, this.old_price, this.price});
 
   @override
   Widget build(BuildContext context) {
-    Container(
-      child: Text(name),
-    );
+    return Card(
+        child: Hero(
+      tag: name,
+      child: Material(
+        child: InkWell(
+          onTap: () => {},
+          child: GridTile(
+            footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    name,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    "\$${price.toInt()}",
+                    style: TextStyle(decoration: TextDecoration.lineThrough),
+                  ),
+                )),
+            child: Container(
+              width: 50,
+              height: 50,
+              child: Image.asset(pictures),
+            ),
+          ),
+        ),
+      ),
+    ));
   }
 }
-
